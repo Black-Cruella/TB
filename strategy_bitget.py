58570233f9b1805f8a312fc9767d670b5df091d7
Black-Cruella <162044591+Black-Cruella@users.noreply.github.com>
2024-03-03 19:36:47 +0100

    Create strategy_bitget.py

---
+++ strategy_bitget.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.append("./live_tools")
+import ccxt
+import ta
+from utilities.perp_bitget import PerpBitget
+import pandas as pd
+import json
+
+account_to_select = "bitget_exemple"
+production = True
+
+pair = "AVAX/USDT:USDT"
+leverage = 1
+type=["long", "short"]
+src="close"
+
+f = open(
+    "./TB/secret.json",
+)
+secret = json.load(f)
+f.close()
+
+
+bitget = PerpBitget(
+    apiKey=secret[account_to_select]["apiKey"],
+    secret=secret[account_to_select]["secret"],
+    password=secret[account_to_select]["password"],
+)
+
+candles = bitget.get_last_historical(pair, "5m", 100)
+df = pd.DataFrame(candles)
+
+# Calculer les bougies Heikin Ashi
+ha_df = pd.DataFrame(index=df.index, columns=['open', 'high', 'low', 'close'])
+
+ha_df['close'] = (df['open'] + df['high'] + df['low'] + df['close']) / 4
+ha_df['close_original'] = df['close']
+
+ha_df.at[ha_df.index[0], 'open'] = df.at[df.index[0], 'close']
+
+for i in range(1, len(df)):
+    ha_df.at[df.index[i], 'open'] = (ha_df.at[df.index[i - 1], 'open'] + ha_df.at[df.index[i - 1], 'close']) / 2
+    ha_df.at[df.index[i], 'high'] = max(df.at[df.index[i], 'high'], ha_df.at[df.index[i], 'open'], ha_df.at[df.index[i], 'close'])
+    ha_df.at[df.index[i], 'low'] = min(df.at[df.index[i], 'low'], ha_df.at[df.index[i], 'open'], ha_df.at[df.index[i], 'close'])
+
+# Afficher le DataFrame des bougies Heikin Ashi
+print(ha_df)
