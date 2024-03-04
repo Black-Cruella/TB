3f277bcfbda19d6b6be508cb69cb13ca757d562f
Black-Cruella <162044591+Black-Cruella@users.noreply.github.com>
2024-03-04 08:14:37 +0100

    Update strategy_bitget.py

--- strategy_bitget.py
+++ strategy_bitget.py
@@ -3,6 +3,7 @@ sys.path.append("./live_tools")
 import ccxt
 import ta
 from perp_bitget import PerpBitget
+import pandas_ta as pda 
 import pandas as pd
 import json
 
@@ -42,6 +43,19 @@ for i in range(1, len(df)):
     ha_df.at[df.index[i], 'open'] = (ha_df.at[df.index[i - 1], 'open'] + ha_df.at[df.index[i - 1], 'close']) / 2
     ha_df.at[df.index[i], 'high'] = max(df.at[df.index[i], 'high'], ha_df.at[df.index[i], 'open'], ha_df.at[df.index[i], 'close'])
     ha_df.at[df.index[i], 'low'] = min(df.at[df.index[i], 'low'], ha_df.at[df.index[i], 'open'], ha_df.at[df.index[i], 'close'])
+
+ST_length = 21
+ST_multiplier = 1.0
+superTrend = pda.supertrend(ha_df['high'], ha_df['low'], ha_df['close'], length=ST_length, multiplier=ST_multiplier)
+ha_df['SUPER_TREND1'] = superTrend['SUPERT_'+str(ST_length)+"_"+str(ST_multiplier)]
+ha_df['SUPER_TREND_DIRECTION1'] = superTrend['SUPERTd_'+str(ST_length)+"_"+str(ST_multiplier)]
+
+ST_length = 14
+ST_multiplier = 2.0
+superTrend = pda.supertrend(ha_df['high'], ha_df['low'], ha_df['close'], length=ST_length, multiplier=ST_multiplier)
+ha_df['SUPER_TREND2'] = superTrend['SUPERT_'+str(ST_length)+"_"+str(ST_multiplier)]
+ha_df['SUPER_TREND_DIRECTION2'] = superTrend['SUPERTd_'+str(ST_length)+"_"+str(ST_multiplier)]
+
 
 # Afficher le DataFrame des bougies Heikin Ashi
 print(ha_df)
