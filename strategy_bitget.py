cc5a49e3f19307f5413a8f9c455f04f2b234b39e
Black-Cruella <162044591+Black-Cruella@users.noreply.github.com>
2024-03-12 08:33:57 +0100

    Update strategy_bitget.py

--- strategy_bitget.py
+++ strategy_bitget.py
@@ -159,7 +159,7 @@ else:
             bitget.place_market_order(pair, "buy", long_quantity, reduce=False)
             stop_loss_price = long_market_price * 0.995  # 0.5% sous le prix d'achat
             print(f"Place Long Stop Loss Order at {stop_loss_price}$")
-            bitget.place_market_order(pair, 'sell', long_quantity, stop_loss_price, reduce=True)
+            bitget.place_limit_order(pair, 'sell', long_quantity, stop_loss_price, stop_loss_price, reduce=True)
 
     elif open_short(row) and "short" in type:
         short_market_price = float(df.iloc[-1]["close"])
@@ -175,7 +175,7 @@ else:
             bitget.place_market_order(pair, "sell", short_quantity, reduce=False)
             stop_loss_price = short_market_price * 1.005  # 0.5% au-dessus du prix de vente
             print(f"Place Short Stop Loss Order at {stop_loss_price}$")
-            bitget.place_market_order(pair, 'buy', short_quantity, stop_loss_price, reduce=True)
+            bitget.place_limit_order(pair, 'buy', short_quantity, stop_loss_price, stop_loss_price, reduce=True)
 
 now = datetime.now()
 current_time = now.strftime("%d/%m/%Y %H:%M:%S")
