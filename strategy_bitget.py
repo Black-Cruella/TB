a68dbeb7c57415e4f6648ceb6595704a0f67a91f
Black-Cruella <162044591+Black-Cruella@users.noreply.github.com>
2024-03-19 09:03:00 +0100

    Update strategy_bitget.py

--- strategy_bitget.py
+++ strategy_bitget.py
@@ -120,7 +120,7 @@ position = [
     {"side": d["side"], "size": float(d["contracts"]) * float(d["contractSize"]), "market_price":d["info"]["marketPrice"], "usd_size": float(d["contracts"]) * float(d["contractSize"]) * float(d["info"]["marketPrice"]), "open_price": d["entryPrice"]}
     for d in positions_data if d["symbol"] == pair]
 
-df['entryPrice'] = entry_price
+df['entryPrice'] = entryPrice
 pd.set_option('display.max_rows', None)
 print(df)
 
