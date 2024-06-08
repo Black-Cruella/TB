ee47a495f3a94699b76db77cc25630a092a6b18e
Black-Cruella <162044591+Black-Cruella@users.noreply.github.com>
2024-06-09 00:37:27 +0200

    Update strategy_bitget_pp.py

--- strategy_bitget_pp.py
+++ strategy_bitget_pp.py
@@ -185,7 +185,7 @@ short_market_price = float(df.iloc[-1]["close"])
 trailing_stop_price = short_market_price * 0.99  # 1% en-dessous du prix de vente
 range_rate = 0.01  # 1% de suivi
 print(f"Place Short Trailing Stop Order at {trailing_stop_price}$ with range rate {range_rate}")
-bitget.place_trailing_stop_order(pair, 'buy', short_quantity, trailing_stop_price, range_rate, reduce=True)
+bitget.place_trailing_stop(pair, 'buy', short_quantity, trailing_stop_price, range_rate, reduce=True)
 
 usd_balance = float(bitget.get_usdt_equity())
 print("USD balance :", round(usd_balance, 2), "$")
