bb585edb91ae040666e857fddccd263de5c1581a
Black-Cruella <162044591+Black-Cruella@users.noreply.github.com>
2024-06-16 20:13:01 +0200

    Update strategy_bitget_pp.py

--- strategy_bitget_pp.py
+++ strategy_bitget_pp.py
@@ -100,7 +100,7 @@ def add_pivots_and_zigzag_to_df(df, dev_threshold, depth):
     df['pivot_low'] = df['pivot_low'].fillna(method='ffill')
     df['price'] = df['price'].fillna(method='ffill')
     # Add new columns if needed
-    df['last_zigzag_price'] = zigzag_df['price'].shift(2)
+    df['last_zigzag_price'] = zigzag_df['price'].shift(1)
     df['last_zigzag_price'] = df['last_zigzag_price'].fillna(method='ffill')
 
     
